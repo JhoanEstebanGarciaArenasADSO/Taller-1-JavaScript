a7e3883cf373740e48d9613905576647b86d88bf
JhoanEstebanGarciaArenasADSO <>
2022-09-14 08:25:11 -0500

    Ejercicio 12

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 11. Escribir un programa que calcule el volumen de un elipsoide
-//V = (4/3) * PI * a * b *c
+// 12. Programa que muestre el pago de una llamada telefónica sabiendo que cada minuto cuesta $355
+// pesos y un IVA de 20%
 
-let a = Number(prompt("Ingrese A "));
-let b = Number(prompt("Ingrese B "));
-let c = Number(prompt("Ingrese c "));
-let vol= (4/3) * Math.PI * a * b * c;
-console.log(`El volumen del elipsoide es: ${vol}`);
+let llama = Number(prompt("Ingrese el tiempo de la llamada telefonica"));
+let min= 355*llama;
+let iva= min*0.20;
+let cost= min+iva;
+console.log(`El costo de la llamda telefonica sin el IVA es: ${min}`);
+console.log(`El costo de la llamda telefonica mas el Iva es: ${cost}`);
 })
