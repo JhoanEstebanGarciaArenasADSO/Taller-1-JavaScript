9fdbbb79293fa69b4aef8cf8f45b3626e45e0bba
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-21 11:56:22 -0500

    Ejercicio 5 Parte 2

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 6. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg
-// transporta ?
+// 5. En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
+// de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
+// nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
+// hacer el bizcocho es
 
-let cajas=25;
-let pesoc=748;
-let pesot=cajas*pesoc;
-console.log(`El camion transporta ${pesot} kilos`);
+let chocolat=20;
+let cacao=10;
+let gramoh=chocolat*cacao;
+console.log(`La cantidad de gramo de harina es: ${gramoh}`);
 })
