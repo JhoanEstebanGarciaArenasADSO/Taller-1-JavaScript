4b60c6cb7c83d80d2b8dc23efe95979cfffa8522
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-21 11:51:59 -0500

    Ejercicio 7 Parte 2

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 4. Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por
-// $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le
-// sobran $91000. ¿cuánto dinero tenía?
+// 7. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
+// 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
+// Lorena?
 
-let llaves=11500*5;
-let bomba=1168000;
-let cajas=87000*3;
-let dinerosobrante=91000;
-let din=llaves+bomba+cajas;
-let dint=din+dinerosobrante;
-console.log(`El precio que pago era: ${din}`);
-console.log(`El dinero que tenia era: ${dint}`);
+let tapas=1;
+let martin=2/3;
+let jairo= 1/4;
+let lorena= tapas-martin - jairo;
+console.log(`A Lorena le corresponde ${lorena} de tapas`);
 })
