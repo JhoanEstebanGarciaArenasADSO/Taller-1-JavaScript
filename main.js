e0dd786bd5af17b1de876b365b5fc0611a5a1d83
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2022-09-28 10:51:56 -0500

    Ejercicio 8 Parte 2

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 5. En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
-// de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
-// nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
-// hacer el bizcocho es
-
-let chocolat=20;
-let cacao=10;
-let gramoh=chocolat*cacao;
-console.log(`La cantidad de gramo de harina es: ${gramoh}`);
+// 8. Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura. Considere que
+// se cobra por m2 y realice el código que representen el algoritmo que le permita ir generando
+// presupuestos para cada cliente.
+let metro = Number(prompt("Ingrese los metros cuadrados que quiere pintar: "))
+let preciom=50000;
+let pre=metro * preciom;
+console.log(`El valor que debe pagar es ${pre} `);
 })
